0d986818899e4f28d59bd53d45fe1b0e790309a3
codegoat23 <codesmrt@proton.me>
2024-12-14 23:13:34 -0800

    this is a first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+const ThingsArray = ["Thing 1","Thing 2"]
+function App() {
+  return (
+    <>
+     <button onclick=>Add item</button>
+    </>
+  );
+}
+
+export default App;
